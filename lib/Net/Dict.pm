2e348fc62050b98f3833c6f9b640b1f91e7172f0
Neil Bowers <neil@bowers.com>
2014-04-04 18:26:00 +0100

    Changed #! path to use env

--- lib/Net/Dict.pm
+++ lib/Net/Dict.pm
@@ -18,7 +18,7 @@ use Net::Cmd;
 use Carp;
 
 use vars qw(@ISA $debug);
-our $VERSION = '2.14';
+our $VERSION = '2.15';
 
 #-----------------------------------------------------------------------
 # Default values for arguments to new(). We also use this to
